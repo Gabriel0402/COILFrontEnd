42f3b24a6171585300e0aa8c0c1510e57ccada68
Chenyang Zhu <zhucy0402@gmail.com>
2017-06-07 22:58:17 +0100

    error message

--- src/app/views/appviews/login.component.ts
+++ src/app/views/appviews/login.component.ts
@@ -37,7 +37,7 @@ export class LoginComponent implements OnInit {
                     this.router.navigate([this.returnUrl]);
                 },
                 error => {
-                    this.alertService.error(error);
+                    this.alertService.error(error.statusText);
                     this.loading = false;
                 });
     }
