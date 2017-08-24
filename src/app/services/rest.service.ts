5bd52af24f0bb89403ddbe0979949ad28123e20a
Chenyang Zhu <zhucy0402@gmail.com>
2017-08-24 21:46:29 +0100

    rest api call

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -5,18 +5,84 @@ import { Http, Headers, RequestOptions, Response } from '@angular/http';
 export class RestService {
     constructor(private http: Http) { }
     private apiurl = "http://php.scripts.psu.edu/kqy1/coil/endpoint.php";
-    getAll() {
-        return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
+
+    //get requests
+
+    getAccounts() {
+        return this.http.get(this.apiurl + '/accounts', this.jwt())
+            .map((response: Response) =>
+                response.json());
     }
 
-    getById(id: number) {
-        return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    getCourses(userId) {
+        return this.http.get(this.apiurl + '/accounts/' + userId + '/courses', this.jwt()).map((response: Response) =>
+            response.json());
     }
-    
-    getAccounts() {
-        return this.http.get(this.apiurl+'/accounts', this.jwt())
-                        .map((response: Response) => 
-                            response.json());
+
+    getRatings(userId) {
+        return this.http.get(this.apiurl + '/accounts/' + userId + '/ratings', this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    getForums(forumId) {
+        return this.http.get(this.apiurl + '/forums/' + forumId + '/forummessages', this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    getForumReply(messageId) {
+        return this.http.get(this.apiurl + '/forummessages/' + messageId + '/replies', this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    getForumRating(messageId) {
+        return this.http.get(this.apiurl + '/ratings/forum/' + messageId, this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    getChatRating(chatId) {
+        return this.http.get(this.apiurl + '/ratings/chat/' + chatId, this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    getCourseForums(courseId) {
+        return this.http.get(this.apiurl + '/courses/' + courseId + '/forums', this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    getCourseRoster(courseId) {
+        return this.http.get(this.apiurl + '/courses/' + courseId + '/rosters', this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    //post requests
+    createAccounts(user) {
+        return this.http.post(this.apiurl + '/accounts', user, this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    createForum(forum) {
+        return this.http.post(this.apiurl + '/forums', forum, this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    postForumMessages(forum) {
+        return this.http.post(this.apiurl + '/forummessages', forum, this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    postForumMessagesReply(messageId,forum) {
+        return this.http.post(this.apiurl + '/forummessages/'+messageId, forum, this.jwt()).map((response: Response) =>
+            response.json());
+    }
+
+    postRatings(rating){
+        return this.http.post(this.apiurl + '/ratings/forum/', rating, this.jwt()).map((response: Response) =>
+        response.json());
+    }
+
+    createCourse(course){
+        return this.http.post(this.apiurl + '/courses', course, this.jwt()).map((response: Response) =>
+        response.json());
     }
 
 
