cf491d2684af04dfccf5cb381ff86fb2ba1c5a89
Chenyang Zhu <zhucy0402@gmail.com>
2017-08-25 18:15:35 +0100

    wire up components

--- src/app/views/appviews/starterview.component.ts
+++ src/app/views/appviews/starterview.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit, } from '@angular/core';
 
-import {RestService} from '../../services/rest.service'
+import {RestService} from '../../services/rest.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx'
 
 @Component({
   selector: 'starter',
@@ -11,13 +13,35 @@ export class StarterViewComponent implements OnDestroy, OnInit  {
 
 public nav:any;
 users:any;
+userId:any;
+courses:any;
+course:any;
+rosters:any
+
 
 public constructor(private restService: RestService) {
+  this.userId='1';
   this.nav = document.querySelector('nav.navbar');
   this.restService.getAccounts().subscribe(data=>
     {
-      this.users=data;}
+      this.users=data.data;
+      console.log(this.users);
+    }
   )
+  this.restService.getCourses(this.userId).subscribe(data=>{
+    this.courses=data.data;
+    this.course=this.courses[0];
+    this.restService.getCourseRoster(this.course.courseId).subscribe(data=>{ 
+      this.rosters=data.data;
+      let Observables = [];
+      for(let user of this.rosters){
+        Observables.push(this.restService.getRatings(user.userId));
+      }
+      Observable.forkJoin(Observables).subscribe(result=>{
+        console.log(result);
+      })
+    })
+  })
 }
 
 public ngOnInit():any {
--- src/app/views/dashboards/dashboard.component.ts
+++ src/app/views/dashboards/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
 
+import {RestService} from '../../services/rest.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx'
 
 declare var jQuery:any;
 
@@ -12,6 +15,35 @@ declare var jQuery:any;
 
 export class DashboardComponent {
 
-
+  users:any;
+  userId:any;
+  courses:any;
+  course:any;
+  rosters:any
+  
+  
+  public constructor(private restService: RestService) {
+    this.userId='1';
+    this.restService.getAccounts().subscribe(data=>
+      {
+        this.users=data.data;
+        console.log(this.users);
+      }
+    )
+    this.restService.getCourses(this.userId).subscribe(data=>{
+      this.courses=data.data;
+      this.course=this.courses[0];
+      this.restService.getCourseRoster(this.course.courseId).subscribe(data=>{ 
+        this.rosters=data.data;
+        let Observables = [];
+        for(let user of this.rosters){
+          Observables.push(this.restService.getRatings(user.userId));
+        }
+        Observable.forkJoin(Observables).subscribe(result=>{
+          console.log(result);
+        })
+      })
+    })
+  }
 
 }
