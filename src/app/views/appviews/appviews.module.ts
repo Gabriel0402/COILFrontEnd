805fcccde7e9242abddc53b6a5ec8831687e249b
Ling Tran <lxiutran@gmail.com>
2018-02-07 14:22:01 -0600

    27 - LT - Introduce common RatingDisplay component to display rating on Home page and beyond

--- src/app/views/appviews/appviews.module.ts
+++ src/app/views/appviews/appviews.module.ts
@@ -1,25 +1,26 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { StarterViewComponent } from "./starterview.component";
-import { InboxComponent } from "./inbox.component";
-import { HotComponent } from "./hot.component";
-import { LoginComponent } from "./login.component";
-import { ForumComponent } from "./forum.component";
-import { ForumMessageComponent } from "./forumMessage.component";
-import { RatingForumMessageComponent } from "./ratingForumMessage.component";
-import { HuskyComponent } from "./huskyViews/husky.component";
-import { HuskyHappiestComponent } from "./huskyViews/huskyHappiest.component";
-import { HuskyHappyComponent } from "./huskyViews/huskyHappy.component";
-import { HuskyNeutralComponent } from "./huskyViews/huskyNeutral.component";
-import { HuskySadComponent } from "./huskyViews/huskySad.component";
-import { HuskyDeadComponent } from "./huskyViews/huskyDead.component";
+import { StarterViewComponent } from './starterview.component';
+import { InboxComponent } from './inbox.component';
+import { HotComponent } from './hot.component';
+import { LoginComponent } from './login.component';
+import { ForumComponent } from './forum.component';
+import { ForumMessageComponent } from './forumMessage.component';
+import { RatingDisplayComponent } from '../../components/rating/ratingDisplay.component';
+import { RatingForumMessageComponent } from './ratingForumMessage.component';
+import { HuskyComponent } from './huskyViews/husky.component';
+import { HuskyHappiestComponent } from './huskyViews/huskyHappiest.component';
+import { HuskyHappyComponent } from './huskyViews/huskyHappy.component';
+import { HuskyNeutralComponent } from './huskyViews/huskyNeutral.component';
+import { HuskySadComponent } from './huskyViews/huskySad.component';
+import { HuskyDeadComponent } from './huskyViews/huskyDead.component';
 
 import { PeityModule } from '../../components/charts/peity';
 import { SparklineModule } from '../../components/charts/sparkline';
-import { SearchPipe } from "./search.pipe";
-import { OrderBy } from "./order.pipe";
+import { SearchPipe } from './search.pipe';
+import { OrderBy } from './order.pipe';
 import { AlertModule } from 'ngx-bootstrap';
 import { RatingModule } from 'ngx-bootstrap';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
@@ -33,6 +34,7 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
     LoginComponent,
     ForumComponent,
     ForumMessageComponent,
+    RatingDisplayComponent,
     RatingForumMessageComponent,
     HuskyComponent,
     HuskyHappiestComponent,
@@ -60,6 +62,7 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
     LoginComponent,
     ForumComponent,
     ForumMessageComponent,
+    RatingDisplayComponent,
     RatingForumMessageComponent,
     HuskyComponent,
     HuskyHappiestComponent,
