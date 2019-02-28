0e6c686ea6da7d186f3cf0b8798e87f9e4ef5d92
nicolewang12 <nicolewang12@gmail.com>
2019-02-28 15:51:22 -0500

    Latex math equation editor integration with quill

--- src/app/views/appviews/forum.component.ts
+++ src/app/views/appviews/forum.component.ts
@@ -30,24 +30,19 @@ export class ForumComponent implements OnDestroy, OnInit {
 
   public editorConfig = {
     toolbar: [
+      [{ 'font': [] }],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
       ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
       //['blockquote', 'code-block'],
-  
       [{ 'header': 1 }, { 'header': 2 }],               // custom button values
       [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
       [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
       [{ 'direction': 'rtl' }],                         // text direction
-  
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-  
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-      [{ 'font': [] }],
       [{ 'align': [] }],
-  
-      ['clean'],                                         // remove formatting button
-  
+      ['clean'], 
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript                                        // remove formatting button
+      ['formula']
       //['link', 'image', 'video']                         // link and image, video
     ]
   };
--- src/app/views/appviews/hot.component.ts
+++ src/app/views/appviews/hot.component.ts
@@ -7,6 +7,7 @@ import 'rxjs/Rx';
 import { UserService } from '../../services/user.service';
 import { FormGroup, FormControl } from '@angular/forms';
 
+
 @Component({
   selector: 'hot',
   templateUrl: 'hot.template.html',
@@ -37,27 +38,22 @@ export class HotComponent implements OnDestroy, OnInit {
 
 public editorConfig = {
     toolbar: [
+      [{ 'font': [] }],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
       ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
       //['blockquote', 'code-block'],
-  
       [{ 'header': 1 }, { 'header': 2 }],               // custom button values
       [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
       [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
       [{ 'direction': 'rtl' }],                         // text direction
-  
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-  
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-      [{ 'font': [] }],
       [{ 'align': [] }],
-  
-      ['clean'],                                         // remove formatting button
-  
+      ['clean'], 
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript                                        // remove formatting button
+      ['formula']
       //['link', 'image', 'video']                         // link and image, video
     ]
-  };
+  }
 
   public constructor(private modalService: BsModalService,
                       private restService: RestService,
